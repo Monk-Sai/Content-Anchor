4731d5cfccbc0089d3f99e0e915f7aec2bf73d88
Super Sai-yan Monk <acharyasaiprasad@gmail.com>
2024-01-21 22:57:05 +0530

    header modifications

--- js/content-anchor.js
+++ js/content-anchor.js
@@ -1,4 +1,8 @@
-function toggleNav() {
-  var nav = document.querySelector('.nav-ul-mobile');
-  nav.classList.toggle('show');
+function showNavHeader(){
+  const sidebar = document.querySelector('.nav-ul-mobile')
+  sidebar.style.display = 'flex'
+}
+function hideNavHeader(){
+  const sidebar = document.querySelector('.nav-ul-mobile')
+  sidebar.style.display = 'none'
 }
