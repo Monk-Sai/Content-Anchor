f6ff6fb2e030f3522338100824cf340edaf01aeb
Super Sai-yan Monk <acharyasaiprasad@gmail.com>
2024-07-27 00:32:30 +0530

    New file alignments

---
+++ js/content-anchor.js
@@ -0,0 +1,8 @@
+function showNavHeader(){
+  const sidebar = document.querySelector('.nav-ul-mobile')
+  sidebar.style.display = 'flex'
+}
+function hideNavHeader(){
+  const sidebar = document.querySelector('.nav-ul-mobile')
+  sidebar.style.display = 'none'
+}
