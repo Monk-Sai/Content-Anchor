392777a80c6fa09bcbaf5a7fb6cb8f07bba55309
Super Sai-yan Monk <125699861+Monk-Sai@users.noreply.github.com>
2024-07-27 00:19:07 +0530

    Delete Content Anchor directory

--- Content Anchor/js/content-anchor.js
+++
@@ -1,8 +0,0 @@
-function showNavHeader(){
-  const sidebar = document.querySelector('.nav-ul-mobile')
-  sidebar.style.display = 'flex'
-}
-function hideNavHeader(){
-  const sidebar = document.querySelector('.nav-ul-mobile')
-  sidebar.style.display = 'none'
-}
